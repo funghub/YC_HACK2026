444e4d69a8700fd674d28dfcb2ee9489bf807e2a
Heather Ho <heatherho@gmail.com>
2026-03-08 11:06:23 -0700

    add samples.sdf.py

--- Drugflow/split_sdf_files.py
+++ Drugflow/split_sdf_files.py
@@ -1,1 +1,9 @@
 # split sdf file
+from rdkit import Chem
+
+suppl = Chem.SDMolSupplier("samples.sdf")
+
+for i, mol in enumerate(suppl):
+    writer = Chem.SDWriter(f"mol_{i+1}.sdf")
+    writer.write(mol)
+    writer.close()
