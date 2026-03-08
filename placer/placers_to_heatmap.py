207756fbc5cfb6d207be7361b21cbeb37a7c3d3a
Solhee Tucker <solheepark29@gmail.comsolheepark29@gmail.comsolheepark29@gmail.com>
2026-03-08 14:36:32 -0700

    2D array place holder

--- placer/placers_to_heatmap.py
+++ placer/placers_to_heatmap.py
@@ -3,7 +3,7 @@ Input: list of 5 csv file names
 Output: json 5*5 table
 
 {
-    "columns": ["RMSD", "Some Score", "C", "D", "E"],
+    "columns": ["top RMSD", "Some Score", "C", "D", "E"],
     "index": ["Ligand1", "Ligand2", "Ligand3", "Ligand4", "Ligand5"],
     "data": [
         [0.5, 0.8, 1.2, 0.9, 1.0],
@@ -22,5 +22,17 @@ import os
 import json
 
 def placers_to_heatmap(folder_path):
-
+    columns = ["top RMSD", "Some Score", "C", "D", "E"]
+    index = ["Ligand1", "Ligand2", "Ligand3", "Ligand4", "Ligand5"]
+    data = [
+        [0.5, 0.8, 1.2, 0.9, 1.0],
+        [0.6, 0.7, 1.1, 0.8, 0.9],
+        [0.4, 0.9, 1.3, 1.0, 1.2],
+        [0.7, 0.6, 1.0, 0.7, 0.8],
+        [0.5, 0.8, 1.2, 0.9, 1.0]
+    ]
+    tb = pd.DataFrame(data, index=index, columns=columns)
     return tb.to_json(orient = 'split')
+
+json_output = placers_to_heatmap("")
+print(json_output)
